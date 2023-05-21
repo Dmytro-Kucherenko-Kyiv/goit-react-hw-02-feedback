8e1954254c3fe9aef0c5544588a2f15eac7a1977
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-05-22 00:13:58 +0300

    add HTML

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+
+        state={{
+          good: 0,
+          neutral: 0,
+          bad: 0
+        }}
+      >
+        <div>
+
+          <h1>Please leave feedback</h1>
+
+          <div>
+            <button>Good</button>
+            <button>Neutral</button>
+            <button>Bad</button>
+          </div>
+
+          <div>
+            <ul>
+              <li>Good:</li>
+              <li>Neutral:</li>
+              <li>Bad:</li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    );
+  };
 };
